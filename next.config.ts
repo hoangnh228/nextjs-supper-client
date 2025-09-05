20716c041646ca19c0e9bc0cf74d9a69e8fbe1bb
Hoang Nguyen <hoang.nguyen@twenty-tech.com>
2025-09-05 13:12:43 +0700

    64. home page

--- next.config.ts
+++ next.config.ts
@@ -6,8 +6,13 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'ik.imagekit.io'
+      },
+      {
+        hostname: 'localhost',
+        pathname: '/**'
       }
-    ]
+    ],
+    qualities: [25, 50, 75, 100]
   }
   /* config options here */
 }
