b2ccfe4b1d1139889c3b6d6a8eedaf6a68690ccd
Hoang Nguyen <hoang.nguyen@twenty-tech.com>
2025-09-05 10:49:21 +0700

    61. update qrcode

--- src/components/qrcode-table.tsx
+++ src/components/qrcode-table.tsx
@@ -2,25 +2,47 @@ import { getTableLink } from '@/lib/utils'
 import { toCanvas } from 'qrcode'
 import { useEffect, useRef } from 'react'
 
-export default function QrcodeTable({ token, tableNumber }: { token: string; tableNumber: number }) {
+export default function QrcodeTable({
+  token,
+  tableNumber,
+  width = 250
+}: {
+  token: string
+  tableNumber: number
+  width?: number
+}) {
   const canvasRef = useRef<HTMLCanvasElement>(null)
 
   useEffect(() => {
-    const canvas = canvasRef.current
-    if (canvas) {
-      toCanvas(
-        canvas,
-        getTableLink({ token, tableNumber }),
-        {
-          width: 150
-        },
-        (error) => {
-          if (error) {
-            console.error(error.message)
-          }
+    const canvas = canvasRef.current!
+    canvas.width = width
+    canvas.height = width + 50
+    const ctx = canvas.getContext('2d')!
+    ctx.fillStyle = 'white'
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+    ctx.font = '16px Arial'
+    ctx.fillStyle = 'black'
+    ctx.fillText(`Bàn ${tableNumber}`, canvas.width / 2.5, canvas.width + 15)
+    ctx.fillText('Quét mã QR để gọi món', canvas.width / 6, canvas.width + 35)
+
+    const vitualCanvas = document.createElement('canvas')
+
+    toCanvas(
+      vitualCanvas,
+      getTableLink({ token, tableNumber }),
+      {
+        width: width,
+        margin: 2
+      },
+      (error) => {
+        if (error) {
+          console.error(error.message)
         }
-      )
-    }
-  }, [token, tableNumber])
+
+        ctx.drawImage(vitualCanvas, 0, 0, width, width)
+      }
+    )
+  }, [token, tableNumber, width])
   return <canvas ref={canvasRef} />
 }
